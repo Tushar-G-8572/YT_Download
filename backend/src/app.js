70bf4618f98117fcec4255ef19b0108e3dcb36d0
Tushar Gupta <tusharg.mp1@gamil.com>
2026-03-24 16:39:35 +0530

    Create Build file

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
 import ytRouter from './routes/yt.route.js';
+import path from 'path';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:'http://localhost:5173'
+}));
 app.use(express.json());
 const DOWNLOADS_DIR = './downloads';
+
+app.use(express.static('./public'));
 
 app.use('/api/yt/downloads', express.static(DOWNLOADS_DIR));
 
 app.use('/api/yt',ytRouter);
+
+app.use("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"));
+})
 
 if (!fs.existsSync(DOWNLOADS_DIR)) fs.mkdirSync(DOWNLOADS_DIR);
 
